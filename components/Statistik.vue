3de65602fa2cf94a8700fb4a1736fe06e98b2c01
Alisafadilah <141700589+Alisafadilah@users.noreply.github.com>
2024-05-03 16:48:24 +0700

    Add files via upload

---
+++ components/Statistik.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <canvas id="statistik"></canvas>
+  </div>  
+</template>
+
+<script setup>
+import Chart from 'chart.js/auto';
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Pengunjung',
+    backgroundColor: [ 
+      'rgb(153, 171, 41)',
+      'rgb(29, 46, 16)',
+      'rgb(197, 43, 43)',
+      'rgb(91, 7, 7)',
+      'rgb(197, 16, 16)',
+      'rgb(39, 177, 255)',
+    ],
+    borderColor: 'rgb(255, 100, 132)',
+    data: [15, 18, 20, 17, 17, 18],
+  }]
+};
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {}
+};
+
+onMounted(() => {
+  const myChart = new Chart(
+  document.getElementById('statistik'),
+  config
+);
+})
+</script>
